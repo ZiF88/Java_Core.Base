febc03a157e05173ce6e86062a57a434dd27189e
Nikita <tsiferov@gmail.com>
2020-12-12 18:41:31 +0300

    Добавил исправления

--- JAVA/src/ru/geekbrains/Homework_5/Employee.java
+++ JAVA/src/ru/geekbrains/Homework_5/Employee.java
@@ -1,15 +1,15 @@
 package ru.geekbrains.Homework_5;
 
 public class Employee {
-    String fullName;
-    String position;
-    String email;
-    int phoneNumber;
-    int salary;
-    int age;
+    private String fullName;
+    private String position;
+    private String email;
+    private int phoneNumber;
+    private int salary;
+    protected int age;
 
 
-    Employee(String fullName, String position, String email, int phoneNumber, int salary, int age) {
+    public Employee(String fullName, String position, String email, int phoneNumber, int salary, int age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
@@ -18,11 +18,11 @@ public class Employee {
         this.age = age;
     }
 
-    int getAge() {
+    public int getAge() {
         return age;
     }
 
-    void printEmployeeInfo() {
+    public void printEmployeeInfo() {
         System.out.printf("ФИО: %s, должность: %s, почта: %s, телефон: %s, зарплата: %s, возраст: %s\n"
                 , fullName, position, email, phoneNumber, salary, age);
     }
