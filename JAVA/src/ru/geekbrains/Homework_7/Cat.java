3b8a771d335903f45380e7470fb1126ff242c829
Nikita <tsiferov@gmail.com>
2020-12-16 01:54:50 +0300

    Сделана версия ДЗ более расширенная. С возможностью повторять кормление. В комментариях указаны недоработки, но уже за пределами задания.

--- JAVA/src/ru/geekbrains/Homework_7/Cat.java
+++ JAVA/src/ru/geekbrains/Homework_7/Cat.java
@@ -1,2 +1,38 @@
-package ru.geekbrains.Homework_7;public class Cat {
+package ru.geekbrains.Homework_7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    private int fill;
+    private int maxFill;
+
+    public Cat(String name, int appetite, int fill, int maxFill) {
+        this.name = name;
+        this.appetite = appetite;
+        this.fill = fill;
+        this.maxFill = maxFill;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void eat(Plate plate) {
+        if (appetite <= plate.getFoodInfo()) {
+            plate.decreaseFood(appetite);
+            this.fill += appetite;
+            System.out.println("Кот " + name + " съел: " + appetite + " еды");
+        } else {
+            System.out.println("Кот " + name + " хочет съесть больше, чем есть в тарелке");
+        }
+    }
+
+    public boolean isHungry() {
+        return (maxFill / 2) <= fill; // кот голоден когда заполнен менее, чем наполовину
+    }
+
+    public int getHungerRate() {
+        return maxFill - fill;
+    }
 }
+
